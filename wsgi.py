440782e44c8d955a10057e85d284d92f9e0cb6a7
Shaswat Kumar <shaswatkr96@gmail.com>
2019-03-27 16:11:55 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -9,8 +9,7 @@ def index():
 
 @app.route("/hello)
     def hello():
-    return render_template(
-      'test.html') < /string:name>
+    return render_template('test.html') < /string:name>
 
 if __name__ == "__main__":
     app.run(host = '0.0.0.0', port = 80)
