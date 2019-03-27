d140545881653b6effc46580c8c27c81049b7612
Shaswat Kumar <shaswatkr96@gmail.com>
2019-03-27 16:34:47 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ from flask import Flask
 application = Flask(__name__)
 @application.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("home.html")
 @application.route("/salvador")
 def salvador():
     return "Hello, Salvador"
