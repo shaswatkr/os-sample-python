1e4cd405ad46582fee38c2b61ade1c546c5e44e4
Shaswat Kumar <shaswatkr96@gmail.com>
2019-03-27 15:35:35 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,20 +2,8 @@ from flask import Flask
 app = Flask(__name__)
  
 @app.route("/")
-def index():
-    return "Index!"
- 
-@app.route("/hello")
 def hello():
-    return "Hello World!"
- 
-@app.route("/members")
-def members():
-    return "Members"
- 
-@app.route("/members/<string:name>/")
-def getMember(name):
-    return name</string:name>
+return "Hello World!"
  
 if __name__ == "__main__":
-    app.run()
+app.run()
