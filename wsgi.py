1ac8aa5946ba1c509c828f1e2c8cd6e560d32696
Shaswat Kumar <shaswatkr96@gmail.com>
2019-03-27 15:54:48 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,8 +2,12 @@ from flask import Flask
 application = Flask(__name__)
 
 @application.route("/")
+def index():
+    return "Index!"
+
+@application.route("/hello")
 def hello():
-    return "Hello World!"
+    return "Hello,World!"
 
 if __name__ == "__main__":
     application.run()
